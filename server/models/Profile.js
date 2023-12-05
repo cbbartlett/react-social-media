fe56726554a0390d86b4324c43a3460d48477aff
Sylvestor <145170477+cbbartlett@users.noreply.github.com>
2023-12-05 13:03:02 -0600

    Edited models files and seeders files

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,5 +1,5 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const profileSchema = new Schema({
   name: {
@@ -38,4 +38,4 @@ profileSchema.methods.isCorrectPassword = async function (password) {
 
 const Profile = model('Profile', profileSchema);
 
-module.exports = Profile;
+export default Profile;
