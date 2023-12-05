4c1a36f42998c5d463f81346b994ca6f58e1d77b
Apollo Vavra <apollolvavra@gmail.com>
2023-12-05 10:46:13 -0600

    added basic functionality in signup and login jsx.

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Perform authentication logic here
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <label>Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
