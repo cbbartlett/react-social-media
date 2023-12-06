65427799ae071f1f81f70ecad72e9478203e9161
Sylvestor <145170477+cbbartlett@users.noreply.github.com>
2023-12-06 11:15:28 -0600

    Put proper MongoDB info in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    `mongodb://${process.env.DB_USERNAME}:${encodeURIComponent(
-        process.env.DB_PASSWORD
-    )}@{process.env.DB_HOST}:27017/${
-      process.env.DB_NAME
-    }?authMechanism=DEFAULT&authSource=${process.env.DB_NAME}`
-);
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
 export default mongoose.connection
